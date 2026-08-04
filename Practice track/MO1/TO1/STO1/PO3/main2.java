e972185df7e229c58c2d530897b3c180b7e9c683
Aman Chauhan <alpha7aman@gmail.com>
2026-08-04 11:32:48 +0530

    practice code printing by print and println

---
+++ Practice track/MO1/TO1/STO1/PO3/main2.java
@@ -0,0 +1,13 @@
+
+public class main2 {
+
+    public static void main(String[] args) {
+        System.out.println("monday");
+        System.out.print("tuesday");
+        System.out.println("wednesday");
+        System.out.println("thrusday");
+        System.out.print("friday");
+        System.out.println("saturday");
+        System.out.println("sunday");
+    }
+}
