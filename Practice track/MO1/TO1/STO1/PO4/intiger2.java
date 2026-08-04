f53b84bde59076c5b3d74eb3494310950fa7fd25
Aman Chauhan <alpha7aman@gmail.com>
2026-08-04 12:29:50 +0530

    if number is intiger

---
+++ Practice track/MO1/TO1/STO1/PO4/intiger2.java
@@ -0,0 +1,15 @@
+
+public class intiger2 {
+
+    public static void main(String[] args) {
+        int number = -12;
+        if (number > 0) {
+            System.out.println("Positive");
+        } else if (number == 0) {
+            System.out.println("zero");
+        } else {
+            System.out.println("Negative");
+        }
+    }
+
+}
