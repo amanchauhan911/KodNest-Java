944c33ba78fb7964ee560e27700c02c98418aebf
Aman Chauhan <alpha7aman@gmail.com>
2026-08-05 12:22:21 +0530

    for loop and itration by terminally in main.java file

---
+++ track/M02/T02/main.java
@@ -0,0 +1,9 @@
+public class main{
+    public static void main(String[] args) {
+        int a =10;
+        int res = a++ + ++a + a++ + a++ + ++a + a++  ;
+        System.out.println(res);
+        System.out.println(a);
+    }
+}
+
---
+++ track/M02/T02/prg.java
@@ -0,0 +1,13 @@
+public class prg{
+    public static void main(String[] args) {
+        int i=1;
+        for(i=1;i<=500;i++){
+            System.out.println(i);
+        }
+
+        while(i>500){
+            i=--i;
+            System.out.println(i);
+        }
+    }
+}
