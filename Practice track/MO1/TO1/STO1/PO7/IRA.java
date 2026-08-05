89e68afe0fada05b96ded538001630003dc374cd
Aman Chauhan <alpha7aman@gmail.com>
2026-08-05 15:28:13 +0530

    Interview Round advisor

---
+++ Practice track/MO1/TO1/STO1/PO7/IRA.java
@@ -0,0 +1,30 @@
+public class IRA {
+    public static void main(String[] args) {
+        int roundCode = 4;
+        int attemptsUsed = 2;
+        int maxAttempts = 3;
+        String roundName = "";
+
+        switch (roundCode) {
+            case 1:
+                roundName = "Aptitude Round";
+                break;
+            case 2:
+                roundName = "Technical Round"; 
+                break;
+            case 3:
+                roundName = "HR Round";
+                break;
+            case 4:
+                roundName = "Final Review";
+                break;
+            default:
+                roundName = "Invalid Round";
+        }
+
+        String attemptstatus = attemptsUsed < 3 ? "Attempt Available" : "Attempt Limit Reached";
+
+        System.out.println("Current Round: " + roundName);
+        System.out.println("Attempt Status: " + attemptstatus);
+    }
+}
