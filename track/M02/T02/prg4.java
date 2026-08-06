04937a181d91f78019f4bd57473ad0ea605529df
Aman Chauhan <alpha7aman@gmail.com>
2026-08-06 11:21:26 +0530

    updating programs types of loops

--- track/M02/T02/prg4.java
+++ track/M02/T02/prg4.java
@@ -1,11 +1,11 @@
 public class prg4{
   public static void main(String[] args) {
-      int j=1;
-      int i=1;
-      for(i=1;i<=5;i++){
-        for(j=1;j<=5;j++){
+      
+      for(int i=1;i<=5;i++){
+        for(int j=1;j<=5;j++){
             System.out.println(j);
         }
+        System.out.println();
       }
   }
 }
