a0b582bbc518eed11cf30bc15517119e027ee2e6
Aman Chauhan <alpha7aman@gmail.com>
2026-08-06 12:49:21 +0530

    break and countinue syntax and workflow in nested loop

---
+++ track/M02/T02/prg9.java
@@ -0,0 +1,14 @@
+public class prg9{
+    //using continue in nested loop
+    public static void main(String[] args){
+        for (int i=1;i<=3;i++){
+            for (int j=1;j<=4;j++){
+                if(j==2){
+                    continue;
+                }
+                System.out.println("i " + i + " j "+j);
+            }
+
+        }
+    }
+}
