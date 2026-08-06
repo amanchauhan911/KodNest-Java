423b27848a92a2b0c3fe89143c0b1237944d3f96
Aman Chauhan <alpha7aman@gmail.com>
2026-08-06 12:29:14 +0530

    break and countinue syntax and workflow

---
+++ track/M02/T02/prg6.java
@@ -0,0 +1,12 @@
+public class prg6{
+
+    //break program syntax and workflow 
+    public static void main(String[] args) {
+      for (int i=1;i<=10;i++){
+        if(i==6){
+            break;
+        }
+        System.out.println(i);
+      }
+    }
+}
