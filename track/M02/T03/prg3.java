c35ee8e037f9fd7b67b3f332bd8954842a093141
Aman Chauhan <alpha7aman@gmail.com>
2026-08-10 10:24:11 +0530

    specific character

---
+++ track/M02/T03/prg3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class prg3{
+    public static void main(String[] args) {
+        
+    
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the char : ");
+    char a = sc.next().charAt(2);
+    System.out.println("the char is :" + a);
+
+    }
+}
