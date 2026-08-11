966714b54c9b7602a50a913cbc86411f984d93b3
Aman Chauhan <alpha7aman@gmail.com>
2026-08-11 11:13:16 +0530

    Emp and empapplication

---
+++ track/M02/T04/Emp.java
@@ -0,0 +1,7 @@
+class Emp{
+    int id;
+    String name;
+    void work(){
+        System.out.println("Working Hard");
+    }
+}
---
+++ track/M02/T04/EmpApplication.java
@@ -0,0 +1,23 @@
+class EmpApplication{
+    public static void main(String[] args) {
+        Emp e1=new Emp();
+        e1.id=11;
+        e1.name="Aman";
+        System.out.println(e1.id);
+        System.out.println(e1.name);
+        e1.work();
+
+        Emp e2;
+        e2=e1;
+        e2.id=13;
+        e2.name="Raju";
+        System.out.println(e1.id);
+        System.out.println(e1.name);
+        System.out.println(e2.id);
+        System.out.println(e2.name);
+        e2.work();
+
+
+
+    }
+}
