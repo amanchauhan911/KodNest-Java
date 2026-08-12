2fb4079f78cbd2c354b5d64d08760ba9284a6e16
Aman Chauhan <alpha7aman@gmail.com>
2026-08-12 13:00:18 +0530

    method overloading

---
+++ track/M02/T04/overloading/CalApplication.java
@@ -0,0 +1,8 @@
+class CalApplication{
+    public static void main(String[] args) {
+        Add a1=new Add();
+        System.out.println(a1.add(10,20));
+        System.out.println(a1.add(10,20.5));
+
+    }
+}
