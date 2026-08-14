ba6ccd4c9791e55f281d621511e21f4580133c7d
Aman Chauhan <alpha7aman@gmail.com>
2026-08-14 11:47:57 +0530

    constructor

---
+++ track/M02/T04/Constructor/Std.java
@@ -0,0 +1,16 @@
+class Std{
+    String name;
+    int age;
+    double height;
+    Std(String name , int age , double height)
+    {
+        this.name=name;
+        this.age=age;
+        this.height=height;
+    }
+    void display(){
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+    }
+}
---
+++ track/M02/T04/Constructor/StdApp.java
@@ -0,0 +1,6 @@
+public class StdApp{
+    public static void main(String[] args){
+        Std s1=new Std("Aman",16,5.10);
+        s1.display();
+    }
+}
