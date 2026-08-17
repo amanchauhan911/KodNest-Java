76f13e6ce2d9a79ceb423a8e89d31c3d84df1302
Aman Chauhan <alpha7aman@gmail.com>
2026-08-17 14:25:40 +0530

    updating repo

---
+++ Practice track/MO1/TO1/STO1/PO8/Aad.java
@@ -0,0 +1,9 @@
+class Aad{
+    int m(short a, short b){
+        return a + b;
+    }
+    int m(int a , int b){
+        return a + b;
+    }
+}
+
---
+++ Practice track/MO1/TO1/STO1/PO8/Adding.java
@@ -0,0 +1,6 @@
+public class Adding{
+    public static void main(String[] args){
+        Aad aq=new Aad();
+        aq.m(a, b);
+    }
+}
