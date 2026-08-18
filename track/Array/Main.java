c2604d68de560b1c956735fccd11d4887d1baf46
Aman Chauhan <alpha7aman@gmail.com>
2026-08-18 12:35:39 +0530

    search element frequncy

--- track/Array/Main.java
+++ track/Array/Main.java
@@ -1,7 +1,9 @@
 public class Main {
+
+    //using the Main file as common for all the classes within the diractory
     public static void main(String[] args) {
-        int arr[]={10,20,30,40,50};
-        ArraySearch as = new ArraySearch();
-        as.search((arr), 12);
+        int arr[]={10,12,30,12,12};
+        Freq f = new Freq();
+        f.count((arr), 12);
     }
 }
