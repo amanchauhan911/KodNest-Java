40a66a2c3a01a08dc50180b8e509c83746a4c802
Aman Chauhan <alpha7aman@gmail.com>
2026-09-07 17:34:31 +0530

    printing length of the String

---
+++ track/M04/Scanner&print/Prg1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Prg1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String learnerName = scanner.nextLine();
+        String city = scanner.nextLine();
+
+        // Display the name, city and name length.
+        System.out.println("Name: " + learnerName);
+        System.out.println("City: " + city);
+        System.out.println("Name length: " + learnerName.length());
+    }
+}
