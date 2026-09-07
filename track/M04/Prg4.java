fb9e7d51967e9d38aa12a8140e090cb83b16fbf8
Aman Chauhan <alpha7aman@gmail.com>
2026-09-07 17:23:40 +0530

    first Strings Programs

---
+++ track/M04/Prg4.java
@@ -0,0 +1,12 @@
+public class Prg4{
+    public static void main(String[] args) {
+        String s3=new String("Java");
+        String s4=new String("Java");
+        if(s3.equals(s4)){
+            System.out.println("Ref are equal");
+        }else{
+            System.out.println("Ref are not equal");
+        }
+
+    }
+}
