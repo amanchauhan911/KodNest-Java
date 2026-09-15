7c2bf92352ddebfc89b3abd645f1109f9f608f55
Aman Chauhan <alpha7aman@gmail.com>
2026-09-15 12:18:51 +0530

    String concepts

--- track/M04/Scanner&print/Prg2.java
+++ track/M04/Scanner&print/Prg2.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Prg2 {
+    //program to count vowels in string
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String text = sc.nextLine();
